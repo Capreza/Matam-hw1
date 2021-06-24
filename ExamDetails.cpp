cb93e9e6ad1663a5bec3b57aa84582a9fb9a518c
Capreza <83648109+Capreza@users.noreply.github.com>
2021-06-24 18:42:01 +0300

    Add files via upload

--- ExamDetails.cpp
+++ ExamDetails.cpp
@@ -1,53 +1,77 @@
-using std::string;
-
-
-class ExamDetails
-{
-
-private:
-    int course_num;
-    int exam_month;
-    int exam_day;
-    double exam_hour;
-    int exam_len;
-    string zoom_link;
-
-public:
-    ExamDetails(int course_num, int exam_month, int exam_day, double exam_hour, int exam_len, string zoom_link = "");
-    ExamDetails(const ExamDetails& examDetails) = default;
-    ~ExamDetails() = default;
-    
-    ExamDetails& operator=(const ExamDetails& exam) = default;
-
-    static ExamDetails makeMatamExam(); //needs to be a reference?
-};
-
+#include "ExamDetails.h"
 
 ExamDetails::ExamDetails(int course_num, int exam_month, int exam_day, double exam_hour, int exam_len, \
 string zoom_link = "") : course_num(course_num), exam_month(exam_month), exam_day(exam_day), exam_hour(exam_hour), \
 exam_len(exam_len), zoom_link(zoom_link)
 {  }
 
-/* ExamDetails::examDetails(const ExamDetails& exam) : course_num(exam.course_num), \
-exam_month(exam.exam_month), exam_day(exam.exam_day), exam_hour(exam.exam_hour), \
-exam_len(exam.exam_len), zoom_link(exam.zoom_link) //do i need to manually copy string?
-{  } */
-
-//does this need to be friend? 
-/* ExamDetails& ExamDetails::operator=(const ExamDetails& examDetails) //do i need to destroy original zoom link?
+string ExamDetails::getLink() const
 {
-    if (this == &examDetails)
+    return zoom_link;
+}
+
+void ExamDetails::setLink(const string& new_link)
+{
+    string copy_link = new_link;
+    zoom_link = copy_link;
+}
+
+
+int ExamDetails::operator-(const ExamDetails& exam) const 
+{
+    int diff_in_days = 0;
+    diff_in_days += exam_day - exam.exam_day;
+    diff_in_days += days_in_month*(exam_month - exam.exam_month) ;
+    return diff_in_days;
+}
+
+bool ExamDetails::operator<(const ExamDetails& exam) const
+{
+    if(exam_month<exam.exam_month)
     {
-        return *this;
+        return true;
     }
-    course_num = examDetails.course_num;
-    exam_month = examDetails.exam_month;
-    exam_day = examDetails.exam_day;
-    exam_hour = examDetails.exam_hour;
-    exam_len = examDetails.exam_len;
-    zoom_link = examDetails.zoom_link;
-    return *this;
-} */
+    else if(exam_month==exam.exam_month)
+    {
+        if(exam_day<exam.exam_day)
+        {
+            return true;
+        }
+        else if(exam_day==exam.exam_day)
+        {
+            if((exam_hour-exam.exam_hour)<epsilon) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const ExamDetails& exam) 
+{
+    int exam_time_round_hour = (int) exam.exam_hour;
+    float exam_time_half_hour = exam.exam_hour - exam_time_round_hour;
+    int exam_time_half_hour_to_print=0;
+    if(exam_time_half_hour>epsilon) 
+    {
+        exam_time_half_hour_to_print=ExamDetails::half_hour;
+    }
+	return os <<"Course Number: "<<exam.course_num<< std::endl<<"Time: "<<exam.exam_day<<"."<<exam.exam_month<<" "<<
+    exam_time_round_hour<<":"<<exam_time_half_hour_to_print<<"0"<<std::endl
+    <<"Duration: "<<std::endl<<"Zoom Link: "<<exam.zoom_link  ;
+}
 
 static ExamDetails ExamDetails::makeMatamExam()
 {
---
+++ ExamDetails.h
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using std::string;
+
+
+class ExamDetails
+{
+
+private:
+    int course_num;
+    int exam_month;
+    int exam_day;
+    double exam_hour;
+    int exam_len;
+    string zoom_link;
+    static const int days_in_month = 30;
+    static const int half_hour = 3;
+    static const double epsilon = 0.000001;
+
+public:
+    ExamDetails(int course_num, int exam_month, int exam_day, double exam_hour, int exam_len, string zoom_link = "");
+    ExamDetails(const ExamDetails& examDetails) = default;
+    ~ExamDetails() = default;
+    string getLink() const;
+    void setLink(const string& new_link);
+    
+    bool operator<(const ExamDetails& exam) const;
+    int operator-(const ExamDetails& exam) const;
+    ExamDetails& operator=(const ExamDetails& exam) = default;
+
+    static ExamDetails makeMatamExam(); //needs to be a reference?
+
+    friend std::ostream& operator<<(std::ostream&, const ExamDetails&);
+};
+
