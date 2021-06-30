1c2e2d1fda210c18d8e5a8509c33b37f46f435a2
Naamasham <83648126+Naamasham@users.noreply.github.com>
2021-06-30 13:05:36 +0300

    Add files via upload

--- SortedList.cpp
+++ SortedList.cpp
@@ -1,194 +1,191 @@
 #include "SortedList.h"
-
-template<class T>
-SortedList<T>::SortedList():size(0),start_node(&(node(null,null)))//not sure exactly how to make node through init list
-{}
-
-template<class T>
-SortedList<T>::SortedList(const SortedList& sorted_list):size(sorted_list.size),start_node(&(node(null,null)))
+#include <stdexcept>
+namespace mtm
 {
-    node *node_to_be_copied = sorted_list.start_node;
-    node *node_to_be_copied_to = start_node;
-    while(node_to_be_copied->next!=null) //need to take care of start node logic
+    template<class T>
+    SortedList<T>::SortedList():size(0),start_node(&(node(null,null)))//not sure exactly how to make node through init list
+    {}
+
+    template<class T>
+    SortedList<T>::SortedList(const SortedList& sorted_list):size(sorted_list.size),start_node(&(node(null,null)))
     {
-        node_to_be_copied=node_to_be_copied->next;
-        node_to_be_copied_to->next = &(node(node_to_be_copied->data,node_to_be_copied->next));
-        node_to_be_copied_to = node_to_be_copied_to->next;
-    }
-}
-
-template<class T>
-SortedList<T>& SortedList<T>::operator=(const SortedList& sorted_list)
-{
-    if(this==&sorted_list)  //this looks weird but i just copied what is in lecture 8 slide 29
-    {
-        return *this;
-    }
-    //not sure if this will cause a memeory leak, dont have a better idea, i think we can just check at debuging
-    start_node = &(node(null,null));
-    size = sorted_list.size;
-    //does this part count as שכפול קוד?
-    node *node_to_be_copied = sorted_list.start_node;
-    node *node_to_be_copied_to = start_node;
-    while(node_to_be_copied->next!=null)
-    {
-        node_to_be_copied=node_to_be_copied->next;
-        node_to_be_copied_to->next = &(node(node_to_be_copied->data,node_to_be_copied->next));
-    }
-    return *this;
-}
-
-template<class T>
-void SortedList<T>::insert(const T& new_element)  //should be class T
-{
-    node *node_to_compare_to = start_node->next;
-    while(node_to_compare_to->data<new_element)
-    {
-        node_to_compare_to = node_to_compare_to->next;
-    }
-    node *node_to_be_after_new_element = node_to_compare_to->next;
-    const int new_element_copy = new_element;//should be class T
-    node_to_compare_to->next = &(node(new_element_copy,node_to_be_after_new_element));
-    size++;
-}
-
-template<class T>
-int SortedList<T>::length()const
-{
-    return size;
-}
-
-template<class T>
-SortedList<T>::node::node(T data,node* next):data(data),next(next)
-{}
-
-template<class T>
-SortedList<T>::const_iterator::const_iterator(const SortedList* const sorted_list, int index): sorted_list(sorted_list), index(index)
-{}
-
-//_______________________________________________________________________________________________________
-
-//_______________________________________________________________________________________________________
-template<class T>
-class SortedList<T>::const_iterator SortedList<T>::begin()const
-{
-    return const_iterator(this,0);
-}
-
-template<class T>
-class SortedList<T>::const_iterator SortedList<T>::end()const
-{
-    return const_iterator(this,size);
-}
-
-template<class T>
-void SortedList<T>::remove(const_iterator iterator)
-{
-    node *node_before_the_one_removed = this->start_node;
-    for(int i = 0 ; i<iterator.index;i++)
-    {
-        node_before_the_one_removed = node_before_the_one_removed->next; //not sure about the math here
-    }
-    node *node_to_remove = node_before_the_one_removed->next;
-    node *node_after_the_one_removed = node_to_remove->next;
-    node_before_the_one_removed->next = node_after_the_one_removed; //really hope this makes the one to remove destroy itself
-}
-
-template<class T>
-template<class Predicate>
-SortedList<T>& SortedList<T>::filter(Predicate pred)
-{
-    SortedList filtered_list;
-    node *current_node_to_filter = this->start_node->next;
-    for(int i=0;i<this->size;i++)
-    {
-        if(pred(current_node_to_filter->data)==true)
+        node *node_to_be_copied = sorted_list.start_node;
+        node *node_to_be_copied_to = start_node;
+        while(node_to_be_copied->next!=null) //need to take care of start node logic
         {
-            filtered_list.insert(current_node_to_filter->data);
+            node_to_be_copied=node_to_be_copied->next;
+            node_to_be_copied_to->next = &(node(node_to_be_copied->data,node_to_be_copied->next));
+            node_to_be_copied_to = node_to_be_copied_to->next;
         }
-        current_node_to_filter = current_node_to_filter->next;
     }
-    return filtered_list;
-}
 
-template<class T>
-template<class Appliance>
-SortedList<T>& SortedList<T>::apply(Appliance app)
-{
-    SortedList applied_list;
-    node *current_node_to_apply = this->start_node->next;
-    for(int i=0;i<this->size;i++)
+    template<class T>
+    SortedList<T>& SortedList<T>::operator=(const SortedList& sorted_list)
     {
-        applied_list.insert(app(current_node_to_apply->data));
-        current_node_to_apply = current_node_to_apply->next;
-    }
-    return applied_list;
-}
-
-
-
-
-
-//_______________________________________________________________________________________________________
-
-//_______________________________________________________________________________________________________
-template<class T>
-SortedList<T>::const_iterator::const_iterator(const const_iterator& iterator): sorted_list(iterator.sorted_list), index(iterator.index)
-{}
-
-template<class T>
-class SortedList<T>::const_iterator& SortedList<T>::const_iterator::operator=(const const_iterator& iterator)
-{
-    if (this == &iterator)
-    {
+        if(this==&sorted_list)  //this looks weird but i just copied what is in lecture 8 slide 29
+        {
+            return *this;
+        }
+        //not sure if this will cause a memeory leak, dont have a better idea, i think we can just check at debuging
+        start_node = &(node(null,null));
+        size = sorted_list.size;
+        //does this part count as שכפול קוד?
+        node *node_to_be_copied = sorted_list.start_node;
+        node *node_to_be_copied_to = start_node;
+        while(node_to_be_copied->next!=null)
+        {
+            node_to_be_copied=node_to_be_copied->next;
+            node_to_be_copied_to->next = &(node(node_to_be_copied->data,node_to_be_copied->next));
+        }
         return *this;
     }
-    index = iterator.index;
-    return *this;
-}
 
-template<class T>
-const T& SortedList<T>::const_iterator::operator*() const 
-{
-    node *current_node = ((this->sorted_list)->start_node);
-    for(int i = 0; i < index; i++)
+    template<class T>
+    void SortedList<T>::insert(const T& new_element)  //should be class T
     {
-        current_node = current_node->next;
+        node *node_to_compare_to = start_node->next;
+        while(node_to_compare_to->data<new_element)
+        {
+            node_to_compare_to = node_to_compare_to->next;
+        }
+        node *node_to_be_after_new_element = node_to_compare_to->next;
+        const int new_element_copy = new_element;//should be class T
+        node_to_compare_to->next = &(node(new_element_copy,node_to_be_after_new_element));
+        size++;
     }
-    return current_node->data;
-}
 
-template<class T>
-class SortedList<T>::const_iterator &SortedList<T>::const_iterator::operator++()
-{
-    if(this->index+1>this->sorted_list->size)
+    template<class T>
+    int SortedList<T>::length()const
     {
-        throw std::out_of_range();
+        return size;
     }
-    ++index;
-    return *this;
-}
 
-template<class T>
-class SortedList<T>::const_iterator SortedList<T>::const_iterator::operator++(int)
-{
-    if(this->index+1>this->sorted_list->size)
+    template<class T>
+    SortedList<T>::node::node(T data,node* next):data(data),next(next)
+    {}
+
+    template<class T>
+    SortedList<T>::const_iterator::const_iterator(const SortedList* const sorted_list, int index): sorted_list(sorted_list), index(index)
+    {}
+
+    //_______________________________________________________________________________________________________
+
+    //_______________________________________________________________________________________________________
+    template<class T>
+    class SortedList<T>::const_iterator SortedList<T>::begin()const
     {
-        throw std::out_of_range();
+        return const_iterator(this,0);
     }
-    const_iterator result = *this;
-    ++*this;
-    return result;
-}
 
-template<class T>
-bool SortedList<T>::const_iterator::operator==(const const_iterator& other_iterator) const
-{
-    return index == other_iterator.index;
-}
+    template<class T>
+    class SortedList<T>::const_iterator SortedList<T>::end()const
+    {
+        return const_iterator(this,size);
+    }
 
-template<class T>
-bool SortedList<T>::const_iterator::operator!=(const const_iterator& other_iterator) const
-{
-    return !(*this == other_iterator);
+    template<class T>
+    void SortedList<T>::remove(const_iterator iterator)
+    {
+        node *node_before_the_one_removed = this->start_node;
+        for(int i = 0 ; i<iterator.index;i++)
+        {
+            node_before_the_one_removed = node_before_the_one_removed->next; //not sure about the math here
+        }
+        node *node_to_remove = node_before_the_one_removed->next;
+        node *node_after_the_one_removed = node_to_remove->next;
+        node_before_the_one_removed->next = node_after_the_one_removed; //really hope this makes the one to remove destroy itself
+    }
+
+    template<class T>
+    template<class Predicate>
+    SortedList<T>& SortedList<T>::filter(Predicate pred)
+    {
+        SortedList filtered_list;
+        node *current_node_to_filter = this->start_node->next;
+        for(int i=0;i<this->size;i++)
+        {
+            if(pred(current_node_to_filter->data)==true)
+            {
+                filtered_list.insert(current_node_to_filter->data);
+            }
+            current_node_to_filter = current_node_to_filter->next;
+        }
+        return filtered_list;
+    }
+
+    template<class T>
+    template<class Appliance>
+    SortedList<T>& SortedList<T>::apply(Appliance app)
+    {
+        SortedList applied_list;
+        node *current_node_to_apply = this->start_node->next;
+        for(int i=0;i<this->size;i++)
+        {
+            applied_list.insert(app(current_node_to_apply->data));
+            current_node_to_apply = current_node_to_apply->next;
+        }
+        return applied_list;
+    }
+
+
+    template<class T>
+    SortedList<T>::const_iterator::const_iterator(const const_iterator& iterator): sorted_list(iterator.sorted_list), index(iterator.index)
+    {}
+
+    template<class T>
+    class SortedList<T>::const_iterator& SortedList<T>::const_iterator::operator=(const const_iterator& iterator)
+    {
+        if (this == &iterator)
+        {
+            return *this;
+        }
+        index = iterator.index;
+        return *this;
+    }
+
+    template<class T>
+    const T& SortedList<T>::const_iterator::operator*() const 
+    {
+        node *current_node = ((this->sorted_list)->start_node);
+        for(int i = 0; i < index; i++)
+        {
+            current_node = current_node->next;
+        }
+        return current_node->data;
+    }
+
+    template<class T>
+    class SortedList<T>::const_iterator &SortedList<T>::const_iterator::operator++()
+    {
+        if(this->index+1>this->sorted_list->size)
+        {
+            throw std::out_of_range();
+        }
+        ++index;
+        return *this;
+    }
+
+    template<class T>
+    class SortedList<T>::const_iterator SortedList<T>::const_iterator::operator++(int)
+    {
+        if(this->index+1>this->sorted_list->size)
+        {
+            throw std::out_of_range();
+        }
+        const_iterator result = *this;
+        ++*this;
+        return result;
+    }
+
+    template<class T>
+    bool SortedList<T>::const_iterator::operator==(const const_iterator& other_iterator) const
+    {
+        return index == other_iterator.index;
+    }
+
+    template<class T>
+    bool SortedList<T>::const_iterator::operator!=(const const_iterator& other_iterator) const
+    {
+        return !(*this == other_iterator);
+    }
 }
--- SortedList.h
+++ SortedList.h
@@ -1,64 +1,66 @@
-
-template<class T>
-class SortedList
+namespace mtm
 {
-    //not sure about division to private and public
-    private:
-    int size;
-    class node;
-    node* start_node;
-    const static int null = 0;
+    template<class T>
+    class SortedList
+    {
+        //not sure about division to private and public
+        private:
+        int size;
+        class node;
+        node* start_node;
+        const static int null = 0;
 
-  
-    //not sure about this part at all
-    template<class Predicate>
-    SortedList& filter(Predicate pred);
-    template<class Appliance>
-    SortedList& apply(Appliance app);
-    //
-    public:
-    class const_iterator;
-    const_iterator begin()const;
-    const_iterator end()const;
-    
-    SortedList();
-    ~SortedList() = default; // not sure about default
-    SortedList(const SortedList& sorted_list);
-    SortedList& operator=(const SortedList& sorted_list);
+        public:
+        class const_iterator;
+        const_iterator begin()const;
+        const_iterator end()const;
+        
+        SortedList();
+        ~SortedList() = default; // not sure about default
+        SortedList(const SortedList& sorted_list);
+        SortedList& operator=(const SortedList& sorted_list);
 
-    void insert(const T& new_element);  
-    void remove(const_iterator iterator);
-    int length()const;
+        void insert(const T& new_element);  
+        void remove(const_iterator iterator);
+        int length()const;
 
-};
+        //not sure about this part at all
+        template<class Predicate>
+        SortedList& filter(Predicate pred);
+        template<class Appliance>
+        SortedList& apply(Appliance app);
+        //
+
+    };
 
 
-template<class T>
-class SortedList<T>::node
-{
-    public:
-    T data;  
-    node* next;
-    node(T data,node* next=null);//should be class T  - also should this be explicit?
-    ~node()=default; // should i add "=default"?
-};
+    template<class T>
+    class SortedList<T>::node
+    {
+        public:
+        T data;  
+        node* next;
+        node(T data,node* next=null);//should be class T  - also should this be explicit?
+        ~node()=default; // should i add "=default"?
+    };
 
-template<class T>
-class SortedList<T>::const_iterator
-{
-    private:
-    const SortedList* const sorted_list;
-    int index;
-    friend class SortedList;
-    //?
-    const_iterator(const SortedList* const sorted_list, int index); //should this be default?
+    template<class T>
+    class SortedList<T>::const_iterator
+    {
+        private:
+        const SortedList* const sorted_list;
+        int index;
+        friend class SortedList;
+        //?
+        const_iterator(const SortedList* const sorted_list, int index); //should this be default?
 
-    public:
-    const_iterator(const const_iterator& iterator);
-    const_iterator& operator=(const const_iterator&);
-    const T& operator*() const;
-    const_iterator& operator++(); // prefix (++it)
-    const_iterator operator++(int);
-    bool operator==(const const_iterator& other_iterator) const;
-    bool operator!=(const const_iterator& other_iterator) const;
-};
+        public:
+        const_iterator(const const_iterator& iterator);
+        const_iterator& operator=(const const_iterator&);
+        const T& operator*() const;
+        const_iterator& operator++(); // prefix (++it)
+        const_iterator operator++(int);
+        bool operator==(const const_iterator& other_iterator) const;
+        bool operator!=(const const_iterator& other_iterator) const;
+    };
+}
