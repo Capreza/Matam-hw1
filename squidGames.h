0fb8e46c16e908f13749741646aee09312794b64
Naamasham <83648126+Naamasham@users.noreply.github.com>
2022-01-03 12:56:24 +0200

    Add files via upload

--- squidGames.h
+++ squidGames.h
@@ -1,6 +1,33 @@
 #ifndef SQUIDGAMES_H
 #define SQUIDGAMES_H
+#include "rankTree.h"
+#include "unionFind.h"
+#include "hashtable.h"
+#include <exception>
 
+class SquidGames
+{
+private:
+    UnionFind Groups;
+    Hashtable Players;
+    int scale;
+    int num_groups; //not necessary but maybe convenient
+
+public:
+    class InvalidError : std::exception {};
+    class Failure : std::exception {};
+
+    SquidGames(int k, int scale) : Groups(k, scale), Players(), scale(scale), num_groups(k) {};
+    ~SquidGames();
+
+    void mergeGroups(int GroupID1, int GroupID2);
+    void addPlayer(int PlayerID, int GroupID, int score);
+    void removePlayer(int PlayerID);
+    void increasePlayerIDLevel(int PlayerID, int LevelIncrease);
+    void changePlayerIDScore(nt PlayerID, int NewScore);
+    void getPercentOfPlayersWithScoreInBounds(int GroupID, int score, int lowerLevel, int higherLevel, double* players);
+    void averageHighestPlayerLevelByGroup(int GroupID, int m, double* avgLevel);
+};
 
 
 #endif //SQUIDGAMES_H
