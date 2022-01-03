82e4d05c3ae66a7f07539eb64a6e60372588b896
Capreza <83648109+Capreza@users.noreply.github.com>
2022-01-03 13:38:55 +0200

    Add files via upload

--- squidGames.cpp
+++ squidGames.cpp
@@ -2,13 +2,14 @@
 
 void SquidGames::mergeGroups(int GroupID1, int GroupID2)
 {
-    if (GroupID1 <= 0 || GroupID2 <= 0 || GroupID1 > num_groups || GroupID2 > num_groups)
-    {
-        throw InvalidError();
-    }
-
-    int first_group = Groups.find(GroupID1);
-    int second_group = Groups.find(GroupID2);
 
 }
 
+void SquidGames::addPlayer(int PlayerID, int GroupID, int score) // this dosnt take into account the super-tree
+{
+    Player* new_player = new Player(PlayerID, GroupID, score);
+    Players.insert(PlayerID,new_player);
+    int group_index = Groups.find(GroupID);
+    RankTree* player_tree = Groups.getGroupTree(group_index);
+    player_tree->getZero()[score-1]++;
+}
